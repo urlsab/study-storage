293c70df81cdb1338ce1a78e770716a392188abc
yair sabag <yairsabag213@gmail.com>
2022-07-06 10:44:18 +0300

    fix navbar router & add button to logout route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+import Home from './components/Home/Home';
+import Search from './components/Search/Search';
+import Register from './components/Register/Register';
+import Login from './components/Login/Login';
+import Logout from './components/Logout/Logout';
+
+import { Route, Switch } from 'react-router-dom';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <main>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/search" component={Search} />
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
+            <Route path="/logout" component={Logout} /> 
+          </Switch>
+      </main>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
+
+
+// Compo's: ::
+// Navbar:
+// -login/logout(if)
+//  Babysitter search
+// Register as babysitter
+
+// Img background for home page
+// Img background all babysitters
+// Img background register
+
+// Home page
+// Sentence wellcome
+// Button register
+// Searchline
+// Button login
